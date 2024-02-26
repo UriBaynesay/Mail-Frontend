61bd2abfb331c041bed400a03253ec4c657da84c
Uri Baynesay <uri552@gmail.com>
2024-02-26 11:36:13 +0200

    move from sidebar to header the logo and hamburger sign

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,22 @@
 export const Header = () => (
   <header className="header-container">
-    <section className="search-bar-container">
-      <h1>search bar</h1>
+    <section className="left-header-container">
+      <section className="logo-container">
+        <div className="hamburger-container">
+          <h4>hamburger</h4>
+        </div>
+        <div className="logo-container">
+          <h4>logo</h4>
+        </div>
+      </section>
+      <section className="search-bar-container">
+        <h1>search bar</h1>
+      </section>
     </section>
-    <div className="user-icon-container">
-      <h1>user icon</h1>
-    </div>
+    <section className="right-header">
+      <div className="user-icon-container">
+        <h1>user icon</h1>
+      </div>
+    </section>
   </header>
 )
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,13 +1,5 @@
 export const Sidebar = () => (
   <section className="sidebar-container">
-    <section className="upper-sidebar-container">
-      <div className="hamburger-container">
-        <h4>hamburger</h4>
-      </div>
-      <div className="logo-container">
-        <h4>logo</h4>
-      </div>
-    </section>
     <button className="compose-btn">compose</button>
     <section className="filters-list-container"><h3>filters list</h3></section>
   </section>
