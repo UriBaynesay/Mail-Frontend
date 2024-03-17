87f040dc7e480115be0ea79ff9f047fb748e9ca5
Uri Baynesay <uri552@gmail.com>
2024-03-17 09:38:58 +0200

    styled compose button

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,10 @@
 export const Sidebar = () => (
   <section className="sidebar-container">
-    <button className="compose-btn">Compose</button>
-    <section className="filters-list-container"><h3>filters list</h3></section>
+    <button className="compose-btn">
+      <h4>Compose</h4>
+    </button>
+    <section className="filters-list-container">
+      <h3>filters list</h3>
+    </section>
   </section>
 )
