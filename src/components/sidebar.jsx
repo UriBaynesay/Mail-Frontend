cd4cc0d3a639a284e81a18b3295d82b1fffc596d
Uri Baynesay <uri552@gmail.com>
2024-03-17 10:03:49 +0200

    added mail types list

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,10 +1,13 @@
+import { MailTypePreview } from "./mail-type-preview"
+const mailTypesList = ["Inbox", "Starred", "Important", "Sent", "Spam"]
+
 export const Sidebar = () => (
   <section className="sidebar-container">
     <button className="compose-btn">
       <h4>Compose</h4>
     </button>
     <section className="filters-list-container">
-      <h3>filters list</h3>
+      {mailTypesList.map((type)=><MailTypePreview type={type}></MailTypePreview>)}
     </section>
   </section>
 )
