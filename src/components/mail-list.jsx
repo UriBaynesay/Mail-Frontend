85fe9bf5cb0c99f9f3d31cb01ec34ef26434c5ce
Uri Baynesay <uri552@gmail.com>
2024-04-02 13:53:52 +0300

    added http request service

--- src/components/mail-list.jsx
+++ src/components/mail-list.jsx
@@ -1,14 +1,14 @@
 import { MailListPreview } from "./mail-list-preview"
-import { SampleData } from "../services/sample-data"
+// import { SampleData } from "../services/sample-data"
 export const MailList = () => (
   <section className="mail-list-container">
     <section className="mail-list-controls-container">
       <h3>hello from controls</h3>
     </section>
     <section className="mail-list-previews-container">
-      {SampleData.mails.map((mail) => (
+      {/* {SampleData.mails.map((mail) => (
         <MailListPreview mail={mail}></MailListPreview>
-      ))}
+      ))} */}
     </section>
   </section>
 )
---
+++ src/services/http.sevice.js
@@ -0,0 +1,33 @@
+import Axios from "axios"
+
+const axios = Axios.create({ withCredentials: true })
+const baseUrl =
+  process.env.CONFIG === "production"
+    ? "/api/"
+    : "http://localhost:3030/api/"
+
+/**
+ *
+ * @param {string} type the type of request get, post, put, delete
+ * @param {string} endpoint the url of the request
+ * @param {*} data
+ * @returns
+ */
+async function sendRequest(type = "get", endpoint, data = {}) {
+  try {
+    const res = await axios({
+      url: baseUrl + endpoint,
+      data,
+      params: type === "get" ? data : {},
+      method: type,
+    })
+    return res.data
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export const httpService = {
+  sendRequest,
+}
