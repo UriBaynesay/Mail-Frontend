e075a407aaf68ad751e912a7dab0bf7977f4500d
Uri Baynesay <uri552@gmail.com>
2024-04-11 11:58:03 +0300

    added message modal and connected it to store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./styles/scss/index.scss"
+import { Provider } from "react-redux"
 import { BrowserRouter as Router } from "react-router-dom"
+
+import "./styles/scss/index.scss"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import { Provider } from "react-redux"
 import { store } from "./store/store"
+import { AppMessage } from "./components/app-message"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
@@ -14,6 +16,7 @@ root.render(
       <Router>
         <App />
       </Router>
+      <AppMessage></AppMessage>
     </Provider>
   </React.StrictMode>
 )
