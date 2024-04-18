fb3015cc95b310a9d2464a8c8e8884c8cf4cf995
Uri Baynesay <uri552@gmail.com>
2024-04-18 12:46:13 +0300

    added logout

--- src/components/homepage/header.jsx
+++ src/components/homepage/header.jsx
@@ -1,50 +1,69 @@
 import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+
+import { authenticationService } from "../../services/authentication.service"
+import { setUser } from "../../store/slices/user";
 import hamburger from "../../styles/svg/hamburger.svg"
 import mail from "../../styles/svg/mail.svg"
 
-export const Header = () => (
-  <header className="header-container">
-    <section className="logo-container">
-      <div className="hamburger-container">
-        <img src={hamburger} alt="" />
-      </div>
-      <div className="logo-image-container">
-        <img src={mail} alt="" />
-      </div>
-      <h1>Mail</h1>
-    </section>
-    <section className="search-bar-container">
-      <form className="search-bar-form-container">
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 26 26"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-          </svg>
-        </button>
-        <input
-          type="text"
-          className="search-bar-input"
-          placeholder="Search mail"
-        />
-      </form>
-    </section>
+export const Header = () => {
+  const user = useSelector((storeState) => storeState.user)
+  const dispatch=useDispatch()
 
-    <section className="user-controls-container">
-      <Link to="login">
-        <div className="user-icon-container">
-          <h1>user icon</h1>
+  const logout = async () => {
+    await authenticationService.logout()
+    dispatch(setUser(null))
+    sessionStorage.removeItem("user")
+  }
+
+  return (
+    <header className="header-container">
+      <section className="logo-container">
+        <div className="hamburger-container">
+          <img src={hamburger} alt="" />
         </div>
-      </Link>
-    </section>
-  </header>
-)
+        <div className="logo-image-container">
+          <img src={mail} alt="" />
+        </div>
+        <h1>Mail</h1>
+      </section>
+      <section className="search-bar-container">
+        <form className="search-bar-form-container">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 26 26"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+          </button>
+          <input
+            type="text"
+            className="search-bar-input"
+            placeholder="Search mail"
+          />
+        </form>
+      </section>
+
+      <section className="user-controls-container">
+        {user ? (
+          <button onClick={logout}>Logout</button>
+        ) : (
+          <Link to="login">
+            <div className="user-icon-container">
+              <h1>user icon</h1>
+            </div>
+          </Link>
+        )}
+      </section>
+    </header>
+  )
+}
